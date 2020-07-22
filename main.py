1376123253afb562e3a9e8ab39d3dc827fb435f7
CodingQuell <ryequellhunter@gmail.com>
2020-07-22 17:21:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+e = random.randint(-100,100)
+found = False
+while found == False: 
+ d = int(input("Guess any number"))
+ if d==e:
+   found = True
+   print("You've Just won!!!")
+          elif d>e:
+  print(str(d), "is more then it")
+elif d<e:
+  print(str(e, "is less then it"))
+else:
+  print("This isnt a number, try again.")
+
